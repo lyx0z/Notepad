82664819938135cbf9a532a3753dfd0dae8372d5
Kucher Mykyta Serhiyovich <mykyta.kucher@digitecgalaxus.ch>
2026-08-18 16:23:16 +0200

    added open and save
    few features

--- Notepad/MainWindow.xaml.cs
+++ Notepad/MainWindow.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System.IO;
-using System.IO.Enumeration;
+﻿using System.Diagnostics;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-
 namespace Notepad;
 
 /// <summary>
@@ -10,31 +8,52 @@ namespace Notepad;
 /// </summary>
 public partial class MainWindow : Window
 {
-    private string? _currentFilePath;
+    private string currentFilePath;
     public MainWindow()
     {
         InitializeComponent();
     }
     private void MenuItem_New_Click(object sender, RoutedEventArgs e)
     {
-        Microsoft.Win32.OpenFolderDialog dialog = new();
-
-        dialog.Multiselect = false;
-        dialog.Title = "Select a folder";
-        
-        bool? result = dialog.ShowDialog();
-        
-        if (result == true)
-        {
-            string fullPathToFolder = dialog.FolderName;
-            string folderNameOnly = dialog.SafeFolderName;
-            // File.Create(fullPathToFolder);
-        }
+        Editor.Clear();
     }
 
     private void OnSaveAs_Clicked(object sender, RoutedEventArgs e)
     {
+        SaveAs();
+    }
 
+    private void OnSave_Clicked(object sender, RoutedEventArgs e)
+    {
+        if (currentFilePath is null)
+        {
+            SaveAs();
+        }
+        File.WriteAllText(currentFilePath, Editor.Text);
+        MessageBox.Show("Successfully saved");
+    }
+
+    private void OnOpenMenuItem_Clicked(object sender, RoutedEventArgs e)
+    {
+        Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+        dlg.DefaultExt = ".txt";
+        dlg.Filter = "Text documents (.txt)|*.txt";
+        bool? result = dlg.ShowDialog();
+        if (result == true)
+        {
+            currentFilePath = dlg.FileName;
+            string openFilePath = dlg.FileName;
+            File.Open(openFilePath, FileMode.Open);
+        }
+    }
+
+    private void OnCloseButton_Clicked(object sender, RoutedEventArgs e)
+    {
+        Environment.Exit(0);
+    }
+
+    private void SaveAs()
+    {
         var dialog = new Microsoft.Win32.SaveFileDialog()
         {
             FileName = "Document", DefaultExt = ".txt", Filter = "Text documents (.txt)|*.txt"
@@ -45,13 +64,7 @@ public partial class MainWindow : Window
         {
             string filePath = dialog.FileName;
             File.WriteAllText(filePath, Editor.Text);
-            _currentFilePath = filePath;    
+            currentFilePath = filePath;     
         }
-        
-    }
-
-    private void OnSave_Clicked(object sender, RoutedEventArgs e)
-    {
-        
     }
 }
