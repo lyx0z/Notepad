074eb5dae1c447aa052161b6e8229a1422ac8d70
Kucher Mykyta Serhiyovich <mykyta.kucher@digitecgalaxus.ch>
2026-08-18 12:56:50 +0200

    New Feature
    Explorer opening

---
+++ Notepad/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Windows;
+namespace Notepad;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void MenuItem_New_Click(object sender, RoutedEventArgs e)
+    {
+        Microsoft.Win32.OpenFolderDialog dialog = new();
+
+        dialog.Multiselect = false;
+        dialog.Title = "Select a folder";
+        
+        bool? result = dialog.ShowDialog();
+        
+        if (result == true)
+        {
+            string fullPathToFolder = dialog.FolderName;
+            string folderNameOnly = dialog.SafeFolderName;
+            // File.Create(fullPathToFolder);
+        }
+    }
+
+    private void OnSaveAs_Clicked(object sender, RoutedEventArgs e)
+    {
+        Microsoft.Win32.OpenFolderDialog dialog = new();
+
+        dialog.Multiselect = false;
+        dialog.Title = "Select a folder";
+        
+        bool? result = dialog.ShowDialog();
+        
+        if (result == true)
+        {
+            string fullPathToFolder = dialog.FolderName;
+            string folderNameOnly = dialog.SafeFolderName;
+            // File.WriteAllLines(Editor);
+        }
+    }
+
+    private void OnSave_Clicked(object sender, RoutedEventArgs e)
+    {
+        
+    }
+}
